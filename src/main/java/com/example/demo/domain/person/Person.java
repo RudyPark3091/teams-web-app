816301fdb6dc7fdb96bfe0f3d86bfefd7a32758d
RudyPark3091 <grayblack313@gmail.com>
2020-12-09 12:56:52 +0900

    Update JPA annotation & schema

--- src/main/java/com/example/demo/domain/person/Person.java
+++ src/main/java/com/example/demo/domain/person/Person.java
@@ -3,11 +3,21 @@ package com.example.demo.domain.person;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
+@Entity
 @AllArgsConstructor
 public class Person {
+    @Id
     private int id;
+
+    @Column
     private String name;
+    @Column
     private String alias;
+    @Column
     private int age;
 }
