605d19a213ee6471b13d74939deb7b3af394e678
RudyPark3091 <grayblack313@gmail.com>
2020-12-19 00:31:05 +0900

    Change leftarea width
    to 300px

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const client = new ApolloClient({
 ReactDOM.render(
   <React.StrictMode>
 		<ApolloProvider client={client}>
-			<App width="400px"/>
+			<App width="300px"/>
 		</ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
